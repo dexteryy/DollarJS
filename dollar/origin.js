fa704a8a01d0b86082a89a8db8ee6b9df0df07fc
dexteryy <dexter.yy@gmail.com>
2014-03-28 19:42:58 +0800

    simplify #children, #contents

--- dollar/origin.js
+++ dollar/origin.js
@@ -205,16 +205,16 @@ define("dollar/origin", [
         children: function(){
             var r = new $();
             this.forEach(function(node){
-                this(r, $(node.children));
-            }, _.merge);
+                _array_push.apply(this, _array_slice.call(node.children));
+            }, r);
             return r;
         },
 
         contents: function(){
             var r = new $();
             this.forEach(function(node){
-                this(r, $(node.childNodes));
-            }, _.merge);
+                _array_push.apply(this, _array_slice.call(node.childNodes));
+            }, r);
             return r;
         },
 
